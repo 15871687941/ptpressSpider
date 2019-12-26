e7429c4f4ba974006eeece31d46573d377a2bcfc
God <15871687941@163.com>
2019-12-26 22:09:40 +0800

    要爬取的字段

--- PTPRESSSPIDER/items.py
+++ PTPRESSSPIDER/items.py
@@ -11,4 +11,19 @@ import scrapy
 class PtpressspiderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    parentCat = scrapy.Field()
+    subCat = scrapy.Field()
+    bookName = scrapy.Field()
+    bookISBN = scrapy.Field()
+    bookAuthor = scrapy.Field()
+    bookAuthorIntro = scrapy.Field()
+    bookIntro = scrapy.Field()
+    bookContentIntro = scrapy.Field()
+    bookDir = scrapy.Field()
+    bookPrice = scrapy.Field()
+    bookDiscountPrice = scrapy.Field()
+    bookPageCount = scrapy.Field()
+    bookPublishTime = scrapy.Field()
+
+
+
